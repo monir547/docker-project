9294c51ab73c542b4a41b0299395052413c246fa
monir547 <moniruzzamanmonir212@gmail.com>
2024-08-25 09:04:48 +0000

    1234rt

---
+++ jellyfin/config/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2024-08-25T09:03:38.0020515Z","EndTimeUtc":"2024-08-25T09:03:38.0079927Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
---
+++ jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2024-08-25T09:03:38.0020455Z","EndTimeUtc":"2024-08-25T09:03:39.5034444Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
