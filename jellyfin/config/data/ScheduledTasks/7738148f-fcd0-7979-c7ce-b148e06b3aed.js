34d6b6d19d38bae06520882c14a0a46ec4bd477c
monir547 <moniruzzamanmonir212@gmail.com>
2024-08-25 10:04:16 +0000

    aSDFVB

--- jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
+++ jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2024-08-25T09:40:55.2350379Z","EndTimeUtc":"2024-08-25T09:41:00.8928525Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
+{"StartTimeUtc":"2024-08-25T10:03:35.1363656Z","EndTimeUtc":"2024-08-25T10:03:53.5305903Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
